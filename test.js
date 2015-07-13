974b766d5598e9f0fe8b019f57afcaa9bec84dda
Alvaro Cabrera <pateketrueke@gmail.com>
2015-07-13 17:57:00 -0500

    Fixed tests

--- test.js
+++ test.js
@@ -1,21 +1,20 @@
 'use strict';
 
 var path = require('path'),
-    y2nw = require('../lib');
-
-var root_dir = path.resolve(__dirname + '/..');
+    y2nw = require('./lib');
 
 y2nw({
   header: 'STATIC = -> "value"',
-  src: root_dir + '/tests',
-  dest: root_dir + '/generated'
+  src: __dirname + '/tests',
+  dest: __dirname + '/generated',
+  steps: __dirname + '/other'
 }, function(err) {
   if (err) {
     console.log(err);
     process.exit(1);
   }
 
-  var suitcase = require(root_dir + '/generated/tests/example-suitcase');
+  var suitcase = require(__dirname + '/generated/tests/example-suitcase');
 
   var browser = {
     end: function() {
